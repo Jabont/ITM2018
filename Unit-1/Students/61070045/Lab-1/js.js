f9fdb044f25eb72ca2cd4fa31ef00f0aac644144
Napat Arayawattanapong <napatkrup@hotmail.com>
2019-01-21 08:58:37 +0700

    edit js snow

--- Unit-1/Students/61070045/Lab-1/js.js
+++ Unit-1/Students/61070045/Lab-1/js.js
@@ -16,15 +16,13 @@ function myFuncalert() {
 }
 
 //My Snow
-const colors = ["#FFFFFF"];
-
 const numSnows = 100;
 const snows = [];
 
 for (let i = 0; i < numSnows; i++) {
 	let snow = document.createElement("div");
 	snow.classList.add("snow");
-	snow.style.background = colors[Math.floor(Math.random() * colors.length)];
+	snow.style.background = "#FFFFFF";
 	snow.style.left = `${Math.floor(Math.random() * 100)}vw`;
 	snow.style.top = `${Math.floor(Math.random() * 100)}vh`;
 	snow.style.transform = `scale(${Math.random()})`;
@@ -34,14 +32,13 @@ for (let i = 0; i < numSnows; i++) {
 	snows.push(snow);
 	document.body.append(snow);
 }
-
-snows.forEach((el, i, ra) => {
+snows.forEach((elementsnow, number) => {
 	let to = {
-		x: Math.random() * (i % 2 === 0 ? -11 : 11),
+		x: Math.random() * (number % 2 === 0 ? -11 : 11),
 		y: Math.random() * 12
 	}
 
-	let anim = el.animate(
+	let animation = elementsnow.animate(
 		[
 			{ transform: "translate(0, 0)" },
 			{ transform: `translate(${to.x}rem, ${to.y}rem)` }
